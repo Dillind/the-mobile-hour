f6c1ef38cec14c23528fbf10b5f832943dd0a180
Dylan Lindsay <dylan.lindsay234@gmail.com>
2023-09-18 21:09:32 +1000

    added orders and staff controllers

--- server.js
+++ server.js
@@ -3,17 +3,21 @@ import express from 'express';
 const app = express();
 const port = 8080;
 
-// TODO: Setup and use session middleware
-// Important because session middleware remembers the user when they navigate between pages.
+// Enable support for URL-encoded request bodies (form posts)
+app.use(express.urlencoded({ extended: true }))
 
-// TODO: Setup and use the EJS view engine
+// Setup and use session middleware (!Importance: session middleware remembers the user when they navigate between pages)
+
+// Setup and use the EJS view engine
+app.set("view engine", "ejs");
 
 // TODO: Setup 404 and root page redirects
 
-// TODO: Setup and use static files middleware
+// Setup and use static files middleware
+app.use(express.static("static"));
 
 // TODO: Import and use controllers
 
 app.listen(port, () => {
-    console.log(`Express server started on http://localhost${port}`)
+    console.log(`Express server started on http://localhost:${port}`)
 })

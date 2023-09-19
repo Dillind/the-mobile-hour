e3682479fd1eba847f3c0b06dc5519c9ca8de000
Dylan Lindsay <dylan.lindsay234@gmail.com>
2023-09-19 16:14:43 +1000

    added samsung galaxy s23 image

--- model_test.js
+++ model_test.js
@@ -1,13 +1,56 @@
+// import {
+//   newStaff,
+//   create,
+//   getAll,
+//   getById,
+//   update,
+//   deleteById,
+// } from "./models/staff.js";
+
 import {
-  newStaff,
+  newProduct,
   create,
   getAll,
   getById,
+  getBySearch,
   update,
-  deleteById,
-} from "./models/staff.js";
+  deleteByID,
+} from "./models/products.js";
 
-const staff = newStaff(null, "Lisa", "H", "User", "leesah", "lisa123");
+// const staff = newStaff(null, "Lisa", "H", "User", "leesah", "lisa123");
+
+const product = newProduct(
+  null,
+  "Apple iPhone 14 Pro",
+  "iPhone 14 Pro",
+  "Apple Inc",
+  "1550",
+  "10",
+  "The iPhone 14 Pro captures incredible detail with its 48MP Main camera, the addition of Dynamic Island allows users to interact with the iPhone differently and the all-day battery life gives you more access to your phone.",
+  1,
+  1
+);
+
+// create(product).then((result) => {
+//   console.log("Query finished running!");
+//   console.log(result);
+// });
+
+// getAll().then((result) => console.log(result));
+
+// getById(2).then((product) => {
+//   product.name = "Samsung Galaxy 14";
+//   product.manufacturer = "Samsung";
+//   product.price = "4000";
+//   product.stock = 20;
+//   update(product)
+//     .then((result) => console.log("Product updated!"))
+//     .catch((error) => console.log("Failed to update..." + error));
+// });
+
+// getBySearch("Samsung")
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));
 
 // create(staff).then((result) => {
 //   console.log("Query finished running!");
---
+++ models/products.js
@@ -0,0 +1,141 @@
+import { db_conn } from "../database.js";
+
+export function newProduct(
+  id,
+  name,
+  model,
+  manufacturer,
+  price,
+  stock,
+  description,
+  last_updated_by_staff_id,
+  feature_id
+) {
+  return {
+    id,
+    name,
+    model,
+    manufacturer,
+    price,
+    stock,
+    description,
+    last_updated_by_staff_id,
+    feature_id,
+  };
+}
+
+// CREATE
+export function create(product) {
+  return db_conn.query(
+    `INSERT INTO products (product_name, product_model, product_manufacturer, product_price, product_stock, product_description, product_last_updated_by_staff_id, product_feature_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?)`,
+    [
+      product.name,
+      product.model,
+      product.manufacturer,
+      product.price,
+      product.stock,
+      product.description,
+      product.last_updated_by_staff_id,
+      product.feature_id,
+    ]
+  );
+}
+
+// READ
+// Get All
+export function getAll() {
+  return db_conn.query(`SELECT * FROM products`).then(([queryResult]) => {
+    // convert each result into a model object
+    return queryResult.map((result) =>
+      newProduct(
+        result.product_id,
+        result.product_name,
+        result.product_model,
+        result.product_manufacturer,
+        result.product_price,
+        result.product_stock,
+        result.product_description,
+        result.product_last_updated_by_staff_id,
+        result.product_feature_id
+      )
+    );
+  });
+}
+
+// Get By Id
+export function getById(productID) {
+  return db_conn
+    .query(`SELECT * FROM products WHERE product_id = ?`, [productID])
+    .then(([queryResult]) => {
+      // checks that at least 1 product was found
+      if (queryResult.length > 0) {
+        const result = queryResult[0];
+
+        // convert the result into a model object
+        return newProduct(
+          result.product_id,
+          result.product_name,
+          result.product_model,
+          result.product_manufacturer,
+          result.product_price,
+          result.product_stock,
+          result.product_description,
+          result.product_last_updated_by_staff_id,
+          result.product_feature_id
+        );
+      } else {
+        return Promise.reject("No matching product found");
+      }
+    });
+}
+
+// Get By Search
+export function getBySearch(searchTerm) {
+  return db_conn
+    .query(
+      `SELECT * FROM products WHERE product_name LIKE ? OR product_description LIKE ?`,
+      [`%${searchTerm}%`, `%${searchTerm}%`]
+    )
+    .then(([queryResult]) => {
+      // convert each result into a model object
+      return queryResult.map((result) =>
+        newProduct(
+          result.product_id,
+          result.product_name,
+          result.product_model,
+          result.product_manufacturer,
+          result.product_price,
+          result.product_stock,
+          result.product_description,
+          result.product_last_updated_by_staff_id,
+          result.product_feature_id
+        )
+      );
+    });
+}
+
+// UPDATE
+export function update(product) {
+  return db_conn.query(
+    `UPDATE products SET product_name = ?, product_model = ?, product_manufacturer = ?, product_price = ?, product_stock = ?, product_description = ?, product_last_updated_by_staff_id = ?, product_feature_id = ? WHERE product_id = ?`,
+    [
+      product.name,
+      product.model,
+      product.manufacturer,
+      product.price,
+      product.stock,
+      product.description,
+      product.last_updated_by_staff_id,
+      product.feature_id,
+      product.id,
+    ]
+  );
+}
+
+// DELETE
+// TODO: Test functionality
+export function deleteByID(productID) {
+  return db_conn.query(`DELETE FROM products WHERE product_id = ?`, [
+    productID,
+  ]);
+}

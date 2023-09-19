a638c4d6b608cf6cb6925f6ae63d7093c68cad89
Dylan Lindsay <dylan.lindsay234@gmail.com>
2023-09-19 11:30:15 +1000

    created CRUD functionalities for staff model

--- model_test.js
+++ model_test.js
@@ -1,8 +1,31 @@
-import { newStaff, create } from "./models/staff.js";
+import {
+  newStaff,
+  create,
+  getAll,
+  getById,
+  update,
+  deleteById,
+} from "./models/staff.js";
 
 const staff = newStaff(null, "Lisa", "H", "User", "leesah", "lisa123");
 
-create(staff).then((result) => {
-  console.log("Query finished running!");
-  console.log(result);
-});
+// create(staff).then((result) => {
+//   console.log("Query finished running!");
+//   console.log(result);
+// });
+
+// getAll().then((result) => console.log(result));
+
+// getById(3)
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));
+
+// getById(2).then((staff) => {
+//   staff.access_role = "manager";
+//   staff.username = "lizer";
+//   update(staff)
+//     .then((result) => console.log("Staff updated!"))
+//     .catch((error) => console.log("Failed to update..." + error));
+// });
+
+// deleteById(2).then((result) => console.log("Staff member deleted!"));
---
+++ models/staff.js
@@ -0,0 +1,111 @@
+import { db_conn } from "../database.js";
+
+// returns an object that gives the information of staff all bundled back together
+export function newStaff(
+  id,
+  first_name,
+  last_name,
+  access_role,
+  username,
+  password
+) {
+  return {
+    id,
+    first_name,
+    last_name,
+    access_role,
+    username,
+    password,
+  };
+}
+
+// CREATE
+export function create(staff) {
+  return db_conn.query(
+    // ?, ?, ?, ?, ? - parameter bindings (placeholders that help prevent sql injection)
+    `INSERT INTO staff (staff_first_name, staff_last_name, staff_role, staff_username, staff_password)
+    VALUES(?, ?, ?, ?, ?)`,
+    [
+      staff.first_name,
+      staff.last_name,
+      staff.access_role,
+      staff.username,
+      staff.password,
+    ]
+  );
+}
+
+// READ
+
+export function getAll() {
+  return (
+    db_conn
+      .query(`SELECT * FROM staff`)
+      // ((([queryResult]))) - picking the format (array destructuring syntax that gets the first format)
+      .then(([queryResult]) => {
+        return queryResult.map((result) =>
+          newStaff(
+            result.staff_id,
+            result.staff_first_name,
+            result.staff_last_name,
+            result.staff_access_role,
+            result.staff_username,
+            result.staff_password
+          )
+        );
+      })
+  );
+}
+
+export function getById(staffId) {
+  return db_conn
+    .query(`SELECT * FROM staff WHERE staff_id = ?`, [staffId])
+    .then(([queryResult]) => {
+      // check that at least one match was found
+      if (queryResult.length > 0) {
+        const result = queryResult[0];
+
+        // Convert the result into a staff member object
+        return newStaff(
+          result.staff_id,
+          result.staff_first_name,
+          result.staff_last_name,
+          result.staff_access_role,
+          result.staff_username,
+          result.staff_password
+        );
+      } else {
+        return Promise.reject("No matching results");
+      }
+    });
+}
+
+// UPDATE
+
+export function update(staff) {
+  return db_conn.query(
+    `
+    UPDATE staff SET
+    staff_first_name = ?,
+    staff_last_name = ?,
+    staff_access_role = ?,
+    staff_username = ?,
+    staff_password = ?
+    WHERE staff_id = ?
+    `,
+    [
+      staff.first_name,
+      staff.last_name,
+      staff.access_role,
+      staff.username,
+      staff.password,
+      staff.id,
+    ]
+  );
+}
+
+// DELETE
+
+export function deleteById(staffId) {
+  return db_conn.query(`DELETE FROM staff WHERE staff_id = ?`, [staffId]);
+}

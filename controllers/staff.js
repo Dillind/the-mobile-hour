e65650790ef1724b71fe9f5a0773127b1d67da06
Dylan Lindsay <dylan.lindsay234@gmail.com>
2023-10-05 14:05:11 +1000

    added error handling, bcrypt and session for login

--- controllers/staff.js
+++ controllers/staff.js
@@ -3,6 +3,7 @@ import express from "express";
 import access_control from "../access_control.js";
 
 import * as Staff from "../models/staff.js";
+import * as Changelog from "../models/changelog.js";
 
 const staffController = express.Router();
 
@@ -16,21 +17,32 @@ staffController.post("/staff_login", (req, res) => {
 
   Staff.getByUsername(login_username)
     .then((staff) => {
-      res.redirect("/order_admin");
+      if (bcrypt.compareSync(login_password, staff.password)) {
+        req.session.user = {
+          staffId: staff.id,
+          accessRole: staff.access_role,
+        };
 
-      // ERROR: causing error, denied access even though user and pw exists.
-      // if (bcrypt.compareSync(login_password, staff.password)) {
-      //   req.session.user = {
-      //     staffID: staff.id,
-      //     accessRole: staff.access_role,
-      //   };
+        // Changelog entry
+        const staffChangelogEntry = Changelog.newChangelog(
+          null,
+          null,
+          req.session.user.staffId,
+          `${staff.username} logged in`
+        );
 
-      // } else {
-      //   res.render("status.ejs", {
-      //     status: "Login Failed",
-      //     message: error,
-      //   });
-      // }
+        Changelog.create(staffChangelogEntry).catch((error) => {
+          console.log("Failed to add to changelog " + staffChangelogEntry);
+        });
+
+        // If successful, redirect to order admin page
+        res.redirect("/order_admin");
+      } else {
+        res.render("status.ejs", {
+          status: "Login Failed",
+          message: "Invalid password",
+        });
+      }
     })
     .catch((error) => {
       res.render("status.ejs", {
@@ -45,33 +57,29 @@ staffController.get("/staff_logout", (req, res) => {
   res.redirect("/");
 });
 
-// need to look into more.
-staffController.get("/staff_admin", (req, res) => {
-  res.render("staff_admin.ejs");
-});
-// staffController.get("/staff_admin", access_control(["manager"]), (req, res) => {
-//   const editID = req.query.edit_id;
+staffController.get("/staff_admin", access_control(["manager"]), (req, res) => {
+  const editID = req.query.edit_id;
 
-//   if (editID) {
-//     Staff.getById(editID).then((editStaff) => {
-//       Staff.getAll().then((allStaff) => {
-//         res.render("staff_admin.ejs", {
-//           allStaff,
-//           editStaff,
-//           accessRole: req.session.user.accessRole,
-//         });
-//       });
-//     });
-//   } else {
-//     Staff.getAll().then((allStaff) => {
-//       res.render("staff_admin.ejs", {
-//         allStaff,
-//         editStaff: Staff.newStaff(0, "", "", "", "", ""),
-//         accessRole: req.session.user.accessRole,
-//       });
-//     });
-//   }
-// });
+  if (editID) {
+    Staff.getById(editID).then((editStaff) => {
+      Staff.getAll().then((allStaff) => {
+        res.render("staff_admin.ejs", {
+          allStaff,
+          editStaff,
+          accessRole: req.session.user.accessRole,
+        });
+      });
+    });
+  } else {
+    Staff.getAll().then((allStaff) => {
+      res.render("staff_admin.ejs", {
+        allStaff,
+        editStaff: Staff.newStaff(0, "", "", "", "", ""),
+        accessRole: req.session.user.accessRole,
+      });
+    });
+  }
+});
 
 // need to look into more to understand.
 staffController.post("/edit_staff", access_control(["manager"]), (req, res) => {
@@ -103,7 +111,6 @@ staffController.post("/edit_staff", access_control(["manager"]), (req, res) => {
   }
 
   // Create a staff model object to represent the staff member submitted
-
   const editStaff = Staff.newStaff(
     formData.staff_id,
     formData.first_name,
@@ -132,6 +139,18 @@ staffController.post("/edit_staff", access_control(["manager"]), (req, res) => {
       res.redirect("/staff_admin");
     });
   }
+
+  // Changelog entry
+  const staffUpdatedChangelogEntry = Changelog.newChangelog(
+    null,
+    null,
+    req.session.user.staffId,
+    `${req.session.user.accessRole} ${formData.action}d staff member with ID ${editStaff.id} `
+  );
+
+  Changelog.create(staffUpdatedChangelogEntry).catch((error) => {
+    console.log("Failed to add to changelog " + staffUpdatedChangelogEntry);
+  });
 });
 
 export default staffController;

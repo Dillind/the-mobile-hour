a1097db1687d097b8e9dfbf5e43d5fb5697aec7d
Dylan Lindsay <dylan.lindsay234@gmail.com>
2023-10-11 19:32:47 +1000

    added async function that tests connection to db

--- database.js
+++ database.js
@@ -6,3 +6,21 @@ export const db_conn = mysql.createPool({
   password: "abc123",
   database: "the-mobile-hour",
 });
+
+async function testDatabaseConnection() {
+  try {
+    const connection = await db_conn.getConnection();
+
+    // If connection is successful, log a success message
+    if (connection) {
+      console.log("Connected to 'the-mobile-hour' database!");
+
+      connection.release();
+    }
+  } catch (error) {
+    // If connection fails, log an error message
+    console.log(`Error connecting to the database ${error}`);
+  }
+}
+
+testDatabaseConnection();

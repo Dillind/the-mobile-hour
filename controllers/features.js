9c1698d4bdb9e1f8fe368d0947bfcf07845d3852
Dylan Lindsay <dylan.lindsay234@gmail.com>
2023-11-07 13:18:42 +1000

    added validation for each of the data inputs

--- controllers/features.js
+++ controllers/features.js
@@ -61,6 +61,108 @@ featureController.post(
     if (req.body) {
       const formData = req.body;
 
+      // validate data formats
+      // feature colour
+      if (!/^[a-zA-Z\s-]{2,}$/.test(formData.feature_color)) {
+        res.render("status.ejs", {
+          status: "Invalid feature colour",
+          message: "Feature colour must be letters",
+        });
+        return;
+      }
+      // feature weight
+      if (!/^\d+(\.\d{1,2})?$/.test(formData.feature_weight)) {
+        res.render("status.ejs", {
+          status: "Invalid feature weight",
+          message: "Feature weight must be numbers only",
+        });
+        return;
+      }
+      // feature dimensions
+      if (
+        !/^(\d+(\.\d+)?\s*x\s*)+\d+(\.\d+)?$/.test(formData.feature_dimensions)
+      ) {
+        res.render("status.ejs", {
+          status: "Invalid feature dimensions",
+          message:
+            "Feature dimensions must be numbers with an 'x' between each number to indicate different lengths",
+        });
+        return;
+      }
+      // feature OS
+      if (!/^[a-zA-Z0-9\s\-,.()]+$/.test(formData.feature_OS)) {
+        res.render("status.ejs", {
+          status: "Invalid feature OS",
+          message: "Feature OS must be letters and numbers",
+        });
+        return;
+      }
+      // feature screensize
+      if (!/^\d+(\.\d{1,2})?$/.test(formData.feature_screensize)) {
+        res.render("status.ejs", {
+          status: "Invalid feature screensize",
+          message: "Feature screensize must be in numbers",
+        });
+        return;
+      }
+      // feature resolution
+      if (!/^\d+\s*x\s*\d+$/.test(formData.feature_resolution)) {
+        res.render("status.ejs", {
+          status: "Invalid feature resolution",
+          message:
+            "Feature resolution must be numbers with an 'x' between each number to indicate different lengths",
+        });
+        return;
+      }
+      // feature CPU
+      if (!/^[a-zA-Z0-9\s\-,.()+*]+$/.test(formData.feature_CPU)) {
+        res.render("status.ejs", {
+          status: "Invalid feature CPU",
+          message: "Feature CPU must be letters and numbers",
+        });
+        return;
+      }
+      // feature RAM
+      if (!/^\d+$/.test(formData.feature_RAM)) {
+        res.render("status.ejs", {
+          status: "Invalid feature RAM",
+          message: "Feature RAM must be numbers",
+        });
+        return;
+      }
+      // feature storage
+      if (!/^\d+$/.test(formData.feature_storage)) {
+        res.render("status.ejs", {
+          status: "Invalid feature storage",
+          message: "Feature storage must be numbers",
+        });
+        return;
+      }
+      // feature battery
+      if (!/^\d+$/.test(formData.feature_battery)) {
+        res.render("status.ejs", {
+          status: "Invalid feature battery",
+          message: "Feature battery must be numbers",
+        });
+        return;
+      }
+      // feature rear camera
+      if (!/^\d+(\.\d{1,2})?$/.test(formData.feature_rear_camera)) {
+        res.render("status.ejs", {
+          status: "Invalid feature rear camera",
+          message: "Feature rear camera must be numbers",
+        });
+        return;
+      }
+      // feature front camera
+      if (!/^\d+(\.\d{1,2})?$/.test(formData.feature_front_camera)) {
+        res.render("status.ejs", {
+          status: "Invalid feature front camera",
+          message: "Feature front camera must be numbers",
+        });
+        return;
+      }
+      // sanitise data formats
       const newFeature = Features.newFeature(
         null,
         validator.escape(formData.feature_color),
@@ -100,6 +202,7 @@ featureController.post(
       });
     } else {
       // Handle error caused if no body exists
+
       res.status(400).send("Missing feature details in request body");
     }
   }
@@ -109,9 +212,9 @@ featureController.get(
   "/product_admin_create_feature",
   access_control(["manager", "user"]),
   (req, res) => {
-    ProductsFeatures.getAll().then((productsFeatures) => {
+    Features.getAll().then((features) => {
       res.render("product_admin_create_feature.ejs", {
-        productsFeatures,
+        features,
         accessRole: req.session.user.accessRole,
       });
     });
@@ -123,6 +226,106 @@ featureController.post(
   access_control(["manager", "user"]),
   (req, res) => {
     const formData = req.body;
+
+    // validate data formats
+    // feature colour
+    if (!/^[a-zA-Z\s-]{2,}$/.test(formData.color)) {
+      res.render("status.ejs", {
+        status: "Invalid feature colour",
+        message: "Feature colour must be letters",
+      });
+      return;
+    }
+    // feature weight
+    if (!/^\d+(\.\d{1,2})?$/.test(formData.weight)) {
+      res.render("status.ejs", {
+        status: "Invalid feature weight",
+        message: "Feature weight must be numbers only",
+      });
+      return;
+    }
+    // feature dimensions
+    if (!/^(\d+(\.\d+)?\s*x\s*)+\d+(\.\d+)?$/.test(formData.dimensions)) {
+      res.render("status.ejs", {
+        status: "Invalid feature dimensions",
+        message:
+          "Feature dimensions must be numbers with an 'x' between each number to indicate different lengths",
+      });
+      return;
+    }
+    // feature OS
+    if (!/^[a-zA-Z0-9\s\-,.()]+$/.test(formData.OS)) {
+      res.render("status.ejs", {
+        status: "Invalid feature OS",
+        message: "Feature OS must be letters and numbers",
+      });
+      return;
+    }
+    // feature screensize
+    if (!/^\d+(\.\d{1,2})?$/.test(formData.screensize)) {
+      res.render("status.ejs", {
+        status: "Invalid feature screensize",
+        message: "Feature screensize must be in numbers",
+      });
+      return;
+    }
+    // feature resolution
+    if (!/^\d+\s*x\s*\d+$/.test(formData.resolution)) {
+      res.render("status.ejs", {
+        status: "Invalid feature resolution",
+        message:
+          "Feature resolution must be numbers with an 'x' between each number to indicate different lengths",
+      });
+      return;
+    }
+    // feature CPU
+    if (!/^[a-zA-Z0-9\s\-,.()+*]+$/.test(formData.CPU)) {
+      res.render("status.ejs", {
+        status: "Invalid feature CPU",
+        message: "Feature CPU must be letters and numbers",
+      });
+      return;
+    }
+    // feature RAM
+    if (!/^\d+$/.test(formData.RAM)) {
+      res.render("status.ejs", {
+        status: "Invalid feature RAM",
+        message: "Feature RAM must be numbers",
+      });
+      return;
+    }
+    // feature storage
+    if (!/^\d+$/.test(formData.storage)) {
+      res.render("status.ejs", {
+        status: "Invalid feature storage",
+        message: "Feature storage must be numbers",
+      });
+      return;
+    }
+    // feature battery
+    if (!/^\d+$/.test(formData.battery)) {
+      res.render("status.ejs", {
+        status: "Invalid feature battery",
+        message: "Feature battery must be numbers",
+      });
+      return;
+    }
+    // feature rear camera
+    if (!/^\d+(\.\d{1,2})?$/.test(formData.rear_camera)) {
+      res.render("status.ejs", {
+        status: "Invalid feature rear camera",
+        message: "Feature rear camera must be numbers",
+      });
+      return;
+    }
+    // feature front camera
+    if (!/^\d+(\.\d{1,2})?$/.test(formData.front_camera)) {
+      res.render("status.ejs", {
+        status: "Invalid feature front camera",
+        message: "Feature front camera must be numbers",
+      });
+      return;
+    }
 
     const editedFeature = Features.newFeature(
       validator.escape(formData.feature_id),

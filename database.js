fcad66868525cdfdbd9bd37ecdf115da1244ae0c
Dylan Lindsay <dylan.lindsay234@gmail.com>
2024-02-13 21:55:53 +1000

    created env file to hide database credentials

--- database.js
+++ database.js
@@ -1,26 +1,39 @@
 import mysql from "mysql2/promise";
+import "dotenv/config";
 
 export const db_conn = mysql.createPool({
-  host: "localhost",
-  user: "tmh-app",
-  password: "abc123",
-  database: "the-mobile-hour",
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
 
-async function testDatabaseConnection() {
-  try {
-    const connection = await db_conn.getConnection();
+// Local Host MySQL Server connection
 
-    // If connection is successful, log a success message
-    if (connection) {
-      console.log("Connected to 'the-mobile-hour' database!");
+// export const db_conn = mysql.createPool({
+//   host: "localhost",
+//   user: "tmh-app",
+//   password: "abc123",
+//   database: "the-mobile-hour",
+// });
 
-      connection.release();
-    }
-  } catch (error) {
-    // If connection fails, log an error message
-    console.log(`Error connecting to the database ${error}`);
-  }
-}
+// async function testDatabaseConnection() {
+//   try {
+//     const connection = await db_conn.getConnection();
 
-testDatabaseConnection();
+//     // If connection is successful, log a success message
+//     if (connection) {
+//       console.log("Connected to 'the-mobile-hour' database!");
+
+//       connection.release();
+//     }
+//   } catch (error) {
+//     // If connection fails, log an error message
+//     console.log(error);
+//   }
+// }
+
+// testDatabaseConnection();

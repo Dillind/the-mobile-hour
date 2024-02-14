c2a4fae8aca2f3bcd087f6c0518e6f736b26ed28
Dylan Lindsay <dylan.lindsay234@gmail.com>
2024-02-14 20:11:37 +1000

    changed dependency to express-session

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from "express";
-// import session from "express-session";
-import session from "cookie-session";
+import session from "express-session";
 
 const app = express();
 const port = 8080;
